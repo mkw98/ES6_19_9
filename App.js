c7bf6b088409acb01f8fcaa43b8a20761056ca73
mkw98 <40563214+mkw98@users.noreply.github.com>
2018-10-25 15:42:27 +0200

    Update App.js

--- App.js
+++ App.js
@@ -36,3 +36,28 @@ class App extends React.Component {
     );
   }
 }
+
+class UsersList extends React.Component {
+  get users() {
+    return this.props.users.map(user => <User key={user.id} user={user}/>);
+  }
+
+  render() {
+    return (
+      <div>
+        {this.users}
+      </div>
+    );
+  }
+}
+
+class User extends React.Component {
+  render() {
+    return (
+      <div>
+        <img src={this.props.user.avatar_url} style={{maxWidth: '100px'}}/>
+        <a href={this.props.user.html_url} target="_blank">{this.props.user.login}</a>
+      </div>
+    );
+  }
+}
