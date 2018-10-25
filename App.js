e9289161ced438f6033e00daf4bc7b6fa9b9c4ac
mkw98 <40563214+mkw98@users.noreply.github.com>
2018-10-25 15:49:49 +0200

    add comments

--- App.js
+++ App.js
@@ -11,13 +11,13 @@ class App extends React.Component {
     this.setState({searchText: event.target.value});
   }
 
-  onSubmit(event) {
+  onSubmit(event) {  //W metodzie onSubmit wykorzystujemy wiedzę na temat promise'ów. 
     event.preventDefault();
     const {searchText} = this.state;
-    const url = `https://api.github.com/search/users?q=${searchText}`;
-    fetch(url)
-      .then(response => response.json())
-      .then(responseJson => this.setState({users: responseJson.items}));
+    const url = `https://api.github.com/search/users?q=${searchText}`; //Po skonsturowaniu adresu URL dzięki szablonom ES6 ...
+    fetch(url) // ...wywołujemy funkcję fetch, która zwraca Promise. Funkcja fetch jest interfejsem dzięki któremu możemy pobierać różne zasoby z sieci (por. obiekt XHR albo inny sposób na korzystanie z AJAX). Fetch jest po prostu natywną implementacją zapytań ajaxowych korzystającą z promisów.
+      .then(response => response.json()) //Kiedy fetch dostanie odpowiedź z serwera (obietnica zostanie spełniona), do pierwszego then trafia obiekt typu Response...
+      .then(responseJson => this.setState({users: responseJson.items})); //który musimy odpowiednio przekształcić na obiekt JSON (stąd metoda response => response.json()). Po tym przekształceniu ustawiamy stan users na tablicę itemsznajdującą się w odpowiedzi od API Githuba.
   }
 
   render() {
